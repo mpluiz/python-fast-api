7a1c7ec479db4fd6b9b928be0da46af886cdb63a
mpluiz <marcospaulo.luiz@gmail.com>
2024-01-10 15:44:00 -0300

    feat: add app main

---
+++ src/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
+from src.config import app_configs, settings
+
+app = FastAPI(**app_configs)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.CORS_ORIGINS,
+    allow_origin_regex=settings.CORS_ORIGINS_REGEX,
+    allow_credentials=True,
+    allow_methods=("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"),
+    allow_headers=settings.CORS_HEADERS,
+)
+
+
+@app.get("/healthcheck", include_in_schema=False)
+async def healthcheck() -> dict[str, str]:
+    return {"status": "ok"}
